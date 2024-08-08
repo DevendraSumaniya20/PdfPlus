b78bda3d721c2f134be3f07ec3b0d091521748d2
Sumaniya Devendra <dsumniya@gmail.com>
2024-08-08 20:56:27 +0530

    add new spalshscreen

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import {LogBox, SafeAreaView, StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useEffect} from 'react';
 import Navigation from './src/navigation/Navigation';
 import {Provider} from 'react-redux';
 import store from './src/redux/store';
-
+import SplashScreen from 'react-native-splash-screen';
 const App = () => {
   LogBox.ignoreLogs([
     'Warning: A props object containing a "key" prop is being spread into JSX:',
   ]);
+
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
 
   return (
     <View style={{flex: 1}}>
